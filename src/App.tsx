75accccd98047f42189a451a61f288dbe3ac292b
=tadangthanh <tathanh.mycv@gmail.com>
2025-04-18 17:00:20 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-yellow-600">
+      Hello Tailwind with TypeScript! ok
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // 👈 bắt buộc với CRA
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
