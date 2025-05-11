3cab9046101140107f9997d589e024fded3336f0
=tadangthanh <tathanh.mycv@gmail.com>
2025-05-11 09:40:14 +0700

    update

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -235,12 +235,14 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
     ]);
     const [isSharedView, setIsSharedView] = useState<boolean>(false);
     useEffect(() => {
-        setActiveLink(location.pathname);
         setItems(prevItems => prevItems.filter(item => !item.includes("parent.id:")));
         setPageNo(0);
         pathRef.current = [{ id: 0, name: isSharedView ? "Kho lưu trữ chia sẻ" : "Kho lưu trữ của tôi" }];
         setFolderId(null);
-    }, [isSharedView, location.pathname])
+    }, [isSharedView])
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location.pathname])
     return (
         <ItemContext.Provider
             value={{
